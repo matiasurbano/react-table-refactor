7c409ad40994fa98e8bd69a7625990e71a418fd3
Matias Ariel Urbano <matias.urbano@gmail.com>
2022-05-21 02:11:34 +1000

    Auto generating data and setting columns

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Table from "../Table";
 import PhoneIcon from "@mui/icons-material/Phone";
+import makeData from "../../services/dataService";
 
 export default function App() {
   const columns = React.useMemo(
@@ -16,19 +17,49 @@ export default function App() {
       {
         Header: "Address",
         accessor: "address"
+      },
+      {
+        Header: "Phone",
+        accessor: "phone"
+      },
+      {
+        Header: "Locales",
+        accessor: "locales"
+      },
+      {
+        Header: "PI name",
+        accessor: "piName"
+      },
+      {
+        Header: "PI-Phone",
+        accessor: "piPhone"
+      },
+      {
+        Header: "Default",
+        accessor: "default"
+      },
+      {
+        Header: "Status",
+        accessor: "status"
+      },
+      {
+        Header: "",
+        accessor: "action"
       }
     ],
     []
   );
-  const data = React.useMemo(
-    () => [
-      { siteId: "123123", name: "MikeA", address: "StreetA 1234" },
-      { siteId: "123125", name: "MikeB", address: "StreetB 1234" },
-      { siteId: "123126", name: "MikeC", address: "StreetC 1234" },
-      { siteId: "1231276", name: "MikeD", address: "StreetD 1234" }
-    ],
-    []
-  );
+  // const data = React.useMemo(
+  //   () => [
+  //     { siteId: "123123", name: "MikeA", address: "StreetA 1234" },
+  //     { siteId: "123125", name: "MikeB", address: "StreetB 1234" },
+  //     { siteId: "123126", name: "MikeC", address: "StreetC 1234" },
+  //     { siteId: "1231276", name: "MikeD", address: "StreetD 1234" }
+  //   ],
+  //   []
+  // );
+
+  const data = React.useMemo(() => makeData(5), []);
 
   return (
     <>
---
+++ src/services/dataService.js
@@ -0,0 +1,38 @@
+import { faker } from "@faker-js/faker";
+
+const range = (len) => {
+  const arr = [];
+  for (let i = 0; i < len; i++) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+const newRecord = () => {
+  const statusChance = Math.random();
+  return {
+    siteId: Math.floor(Math.random() * 10000),
+    name: faker.name.findName(),
+    address: faker.address.countryCode(),
+    phone: faker.phone.phoneNumber(),
+    locales: "es_US",
+    piName: "",
+    piPhone: "N/A / N/A",
+    default: "",
+    status: statusChance > 0.66
+  };
+};
+
+export default function makeData(...lens) {
+  const makeDataLevel = (depth = 0) => {
+    const len = lens[depth];
+    return range(len).map((d) => {
+      return {
+        ...newRecord(),
+        subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined
+      };
+    });
+  };
+
+  return makeDataLevel();
+}
